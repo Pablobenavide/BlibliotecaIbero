c77d2255c4d36811f1d0a1617e0b524384b1743f
Pablobenavide <99214710+Pablobenavide@users.noreply.github.com>
2025-06-06 21:57:52 -0500

    Update TipoUsuarioEnum.java
    Creación de comentarios explicativos para clase tipo de usuarios.

--- src/model/TipoUsuarioEnum.java
+++ src/model/TipoUsuarioEnum.java
@@ -1,6 +1,9 @@
 package model;
 
+// Enumeración que define los tipos de usuario en el sistema
 public enum TipoUsuarioEnum {
     
-    ADMINISTRADOR, USUARIO;
+    // Valores posibles para un usuario
+    ADMINISTRADOR, // Usuario con privilegios administrativos
+    USUARIO        // Usuario estándar o común
 }
